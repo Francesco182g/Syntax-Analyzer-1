c851c0081b9bb10955ae7540731252be80958f03
Francesco Garofalo <francescogarofalo182@live.com>
2018-11-25 23:59:09 +0100

    Parser Struttura

--- Syntax-Analyzer-1/src/Parser.java
+++ Syntax-Analyzer-1/src/Parser.java
@@ -2,10 +2,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class Parser {
 	
 	private static Scanner in;
+	static int prt;
+	public final static Logger log = Logger.getLogger(Lexer.class.getName());
+
 	public static void main (String[] args) {
 		String lessema = null;
 		try {
@@ -15,11 +19,9 @@ public class Parser {
 		}
 		Lexer lexer = new Lexer();
 		lexer.getNextToken(lessema);
+		prt = 0;
 	}
 
-	public static void getNextToken(Token token) {
-		System.out.println(token);
-	}
 	
 	public static String leggiFile() throws FileNotFoundException {
 	    File file= new File("C:\\Users\\Francesco\\Desktop\\Compilatori\\"+"Testo.txt");
@@ -32,7 +34,44 @@ public class Parser {
 		return lessema;
 	  }
 	
-	public static void printToken(Token token) {
-		System.out.println(token.toString());
+/*	GRAMMATICA:
+ * 
+ * 	Program -> Stmt A
+ * 	A -> ; Stmt A
+ * 	A -> 3(empty)
+ * 	Stmt -> IF Expr THEN Stmt
+ * 	Stmt -> ID ASSIGN Expr
+ * 	Expr -> Term RELOP Term
+ * 	Expr -> Term
+ * 	Term -> ID
+ * 	Term -> NUMBER
+ */
+
+	
+	static boolean Program(){	
+		
+		return true;
 	}
+	
+	 static boolean A(){
+		return false;
+	 }
+	 
+	 static boolean Stmt() {
+		return false;
+	 }
+	 
+	 static boolean Expr() {
+		 return false;
+	 }
+	 
+	 static boolean Term() {
+		 return false;
+	 }
+	
+	
+	public static void getNextToken(Token token) {
+		
+	}
+	
 }
